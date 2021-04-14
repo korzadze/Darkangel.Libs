0391a65be8aab1161440d5fba4f86b9084937371
Александр Корзадзе <korzadze@outlook.com>
2021-04-15 02:56:36 +0300

    Начало библиотеки обработки коммандной строки

---
+++ Darkangel.CommandLine/OptionFlags.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Darkangel
+{
+    /// <summary>
+    /// <para>Допустимость наличия значения для опции</para>
+    /// </summary>
+    [Flags]
+    public enum OptionFlags
+    {
+        /// <summary>
+        /// <para>Параметр командной строки не является опцией</para>
+        /// </summary>
+        NonOption = 1 << 0,
+        /// <summary>
+        /// <para>Значение не допустимо</para>
+        /// </summary>
+        ValueDenied = 1 << 1,
+        /// <summary>
+        /// <para>Значение обязательно</para>
+        /// </summary>
+        ValueRequired = 1 << 2,
+        /// <summary>
+        /// <para>Значение допустимо</para>
+        /// </summary>
+        ValueOptional = ValueRequired | ValueDenied,
+    }
+}
