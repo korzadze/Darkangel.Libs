3d7a1a6e801bb104c6e08f74b9f3a66a223c8161
Александр Корзадзе <korzadze@outlook.com>
2021-04-19 00:47:26 +0300

    Пара идей, может, пустых...

--- Darkangel.Reflection/ReflectionHelper.cs
+++ Darkangel.Reflection/ReflectionHelper.cs
@@ -3,8 +3,9 @@
     /// <summary>
     /// <para>Утилиты для работы с отражениями</para>
     /// </summary>
+    /// <remarks>2021-04-18</remarks>
     public static partial class ReflectionHelper
     {
-
+        //
     }
 }
