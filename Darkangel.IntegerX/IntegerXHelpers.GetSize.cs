b52e1b475657106d09eb70d83ba3c69772e23dbb
Александр Корзадзе <korzadze@outlook.com>
2021-04-28 16:45:54 +0300

    Добавлен GetSize()

---
+++ Darkangel.IntegerX/IntegerXHelpers.GetSize.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Darkangel.IntegerX
+{
+    /// <summary>
+    /// <para>Вспомогательные утилиты для целочисленных операций</para>
+    /// </summary>
+    public static partial class IntegerX
+    {
+        /// <summary>
+        /// <para>Получить размер целочисленного типа в терминах библиотеки</para>
+        /// </summary>
+        /// <param name="type">Целочисленный тип</param>
+        /// <returns>Размер значения типа или -1, для неизвестных типов</returns>
+        public static int GetSize(Type type)
+        {
+            return type.FullName switch
+            {
+                "System.Byte" => IntegerXHelpers.UInt8_ByteSize,
+                "System.SByte" => IntegerXHelpers.Int8_ByteSize,
+                "System.UInt16" => IntegerXHelpers.UInt16_ByteSize,
+                "System.Int16" => IntegerXHelpers.Int16_ByteSize,
+                "Darkangel.IntegerX.UInt24" => IntegerXHelpers.UInt24_ByteSize,
+                "System.UInt32" => IntegerXHelpers.UInt32_ByteSize,
+                "System.Int32" => IntegerXHelpers.Int32_ByteSize,
+                "Darkangel.IntegerX.UInt48" => IntegerXHelpers.UInt48_ByteSize,
+                "System.UInt64" => IntegerXHelpers.UInt64_ByteSize,
+                "System.Int64" => IntegerXHelpers.Int64_ByteSize,
+                _ => -1,
+            };
+        }
+        /// <summary>
+        /// <para>Получить размер целочисленного типа в терминах библиотеки</para>
+        /// </summary>
+        /// <typeparam name="T">Целочисленный тип</typeparam>
+        /// <returns>Размер значения типа или -1, для неизвестных типов</returns>
+        public static int GetSize<T>()
+        {
+            return GetSize(typeof(T));
+        }
+    }
+}
--- Darkangel.IntegerX/IntegerXHelpers.cs
+++ Darkangel.IntegerX/IntegerXHelpers.cs
@@ -6,15 +6,15 @@
     /// <remarks>2021-04-18</remarks>
     public static partial class IntegerXHelpers
     {
-        private const int Int8_ByteSize = 1;
-        private const int UInt8_ByteSize = 1;
-        private const int Int16_ByteSize = 2;
-        private const int UInt16_ByteSize = 2;
-        private const int UInt24_ByteSize = 3;
-        private const int Int32_ByteSize = 4;
-        private const int UInt32_ByteSize = 4;
-        private const int UInt48_ByteSize = 6;
-        private const int Int64_ByteSize = 8;
-        private const int UInt64_ByteSize = 8;
+        internal const int Int8_ByteSize = 1;
+        internal const int UInt8_ByteSize = 1;
+        internal const int Int16_ByteSize = 2;
+        internal const int UInt16_ByteSize = 2;
+        internal const int UInt24_ByteSize = 3;
+        internal const int Int32_ByteSize = 4;
+        internal const int UInt32_ByteSize = 4;
+        internal const int UInt48_ByteSize = 6;
+        internal const int Int64_ByteSize = 8;
+        internal const int UInt64_ByteSize = 8;
     }
 }
