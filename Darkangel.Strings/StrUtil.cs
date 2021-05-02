99ece03dbcb2869994a5af1ce58af65258fd4dd3
Александр Корзадзе <korzadze@outlook.com>
2021-05-02 12:15:44 +0300

    очистка кода

--- Darkangel.Strings/StrUtil.cs
+++ Darkangel.Strings/StrUtil.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Darkangel.Strings
+﻿namespace Darkangel.Strings
 {
     /// <summary>
     /// <para>Утилиты для работы со строками стандартной библиотеки C/C++</para>
