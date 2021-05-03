faba955ab195314d5f5c453fd2bc9d12d3bbf7ec
Александр Корзадзе <korzadze@outlook.com>
2021-05-03 13:23:59 +0300

    Фиксы XmlHelper-ов

--- Darkangel.Xml/XmlHelpers.AddChild.cs
+++ Darkangel.Xml/XmlHelpers.AddChild.cs
@@ -65,5 +65,65 @@ namespace Darkangel.Xml
             root.AppendChild(child);
             return child;
         }
+        /// <summary>
+        /// <para>Добавить элемент к документу</para>
+        /// </summary>
+        /// <param name="doc"><para>XML документ</para></param>
+        /// <param name="childName"><para>Имя дочернего элемента</para></param>
+        /// <returns><para>Добавленый дочерний элемент</para></returns>
+        /// <remarks>2021-04-18</remarks>
+        public static XmlElement AddChild(this XmlDocument doc, string childName)
+        {
+            #region Проверка аргументов
+#if CHECK_ARGS
+            _ = doc ?? throw new ArgumentNullException(nameof(doc));
+            _ = childName ?? throw new ArgumentNullException(nameof(childName));
+#endif
+            #endregion Проверка аргументов
+            XmlElement child = doc.CreateElement(childName);
+            doc.AppendChild(child);
+            return child;
+        }
+        /// <summary>
+        /// <para>Добавить элемент к документу</para>
+        /// </summary>
+        /// <param name="doc"><para>XML документ</para></param>
+        /// <param name="qualifiedName"><para>Полное имя элемента.</para><para>Если имя содержит двоеточие, свойство Prefix отражает часть имени, предшествующую ему, а свойство LocalName — ту часть, которая следует за двоеточием. Полное имя не может содержать префикс "xmlns".</para></param>
+        /// <param name="namespaceUri"><para>Универсальный код ресурса (URI) пространства имен элемента.</para></param>
+        /// <returns><para>Добавленый дочерний элемент</para></returns>
+        /// <remarks>2021-04-18</remarks>
+        public static XmlElement AddChild(this XmlDocument doc, string qualifiedName, string namespaceUri)
+        {
+            #region Проверка аргументов
+#if CHECK_ARGS
+            _ = doc ?? throw new ArgumentNullException(nameof(doc));
+            _ = qualifiedName ?? throw new ArgumentNullException(nameof(qualifiedName));
+#endif
+            #endregion Проверка аргументов
+            XmlElement child = doc.CreateElement(qualifiedName, namespaceUri);
+            doc.AppendChild(child);
+            return child;
+        }
+        /// <summary>
+        /// <para>Добавить элемент к документу</para>
+        /// </summary>
+        /// <param name="doc"><para>XML документ</para></param>
+        /// <param name="prefix">Префикс нового элемента (если имеется). String.Empty равнозначно значению null.</param>
+        /// <param name="localName">Локальное имя дочернего элемента.</param>
+        /// <param name="namespaceUri"><para>Универсальный код ресурса (URI) пространства имен элемента.</para></param>
+        /// <returns><para>Добавленый дочерний элемент</para></returns>
+        /// <remarks>2021-04-18</remarks>
+        public static XmlElement AddChild(this XmlDocument doc, string prefix, string localName, string namespaceUri)
+        {
+            #region Проверка аргументов
+#if CHECK_ARGS
+            _ = doc ?? throw new ArgumentNullException(nameof(doc));
+            _ = localName ?? throw new ArgumentNullException(nameof(localName));
+#endif
+            #endregion Проверка аргументов
+            XmlElement child = doc.CreateElement(prefix, localName, namespaceUri);
+            doc.AppendChild(child);
+            return child;
+        }
     }
 }
--- Darkangel.Xml/XmlHelpers.AddText.cs
+++ Darkangel.Xml/XmlHelpers.AddText.cs
@@ -23,5 +23,38 @@ namespace Darkangel.Xml
             element.AppendChild(te);
             return element;
         }
+        /// <summary>
+        /// <para>Добавить описание к документу, если его нет</para>
+        /// </summary>
+        /// <param name="doc">XML документ</param>
+        /// <param name="version">Версия XML</param>
+        /// <param name="encoding">Кодировка</param>
+        /// <param name="standalone">Игнорировать правила markup DTD</param>
+        /// <returns>Исходный XML документ</returns>
+        public static XmlDocument AddDeclaration(this XmlDocument doc, string version = null, string encoding = null, string standalone = null)
+        {
+            version ??= "1.0";
+            encoding ??= "utf-8";
+            standalone ??= "yes";
+            if (doc.ChildNodes.Count > 0)
+            {//Есть элементы
+                if (doc.FirstChild is XmlDeclaration)
+                {// Уже есть декларация
+                    return doc;
+                }
+                else
+                {//Еще нет декларации
+                    var decl = doc.CreateXmlDeclaration(version, encoding, standalone);
+                    doc.InsertBefore(decl, doc.FirstChild);
+                    return doc;
+                }
+            }
+            else
+            {//Нет элементов
+                var decl = doc.CreateXmlDeclaration(version, encoding, standalone);
+                doc.AppendChild(decl);
+                return doc;
+            }
+        }
     }
 }
