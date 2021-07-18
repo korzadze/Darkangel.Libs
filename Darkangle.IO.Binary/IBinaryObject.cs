afe72dd62fdce9180981bcbe185ef508f2a5e868
Александр Корзадзе <korzadze@outlook.com>
2021-07-18 20:43:52 +0300

    мелкие фиксы

--- Darkangle.IO.Binary/IBinaryObject.cs
+++ Darkangle.IO.Binary/IBinaryObject.cs
@@ -14,11 +14,5 @@ namespace Darkangel.IO
         /// <param name="context">Контекст хранения двоичных данных в потоке</param>
         /// <returns>Размер объекта в потоке</returns>
         long GetSize(IBinaryContext context);
-        /// <summary>
-        /// <para>Получить размер объекта в потоке</para>
-        /// </summary>
-        /// <param name="context">Контекст хранения двоичных данных в потоке</param>
-        /// <returns>Размер объекта в потоке</returns>
-        Task<long> GetSizeAsync(IBinaryContext context);
     }
 }
